4c93605124d9799e2bba4654e06caf93c7b85424
300932732 <300932732@PR-A315-22>
2017-07-11 15:48:21 -0400

    Add Power class

---
+++ COMP123-S2017-Lesson9A/Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: chanpreet mudhar
+ * Date: July 11, 2017
+ * Description: This is the new power data type
+ * derive from
+ * Version: 0.1 Power class created
+ */
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// The power class contains the data and structure of each superclass
+    /// </summary>
+    public class Power
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
